7681935072397fa66532f2952cc3306924769f32
zjl <512503118@qq.com>
2023-05-27 15:19:37 +0800

    完成了开机启动功能

--- RemoteCtrl/RemoteCtrl/RemoteCtrl.cpp
+++ RemoteCtrl/RemoteCtrl/RemoteCtrl.cpp
@@ -24,6 +24,11 @@ using namespace std;
 
 void ChooseAutoInvoke()
 {
+	CString strPath =(_T("C:\\Windows\\SysWOW64\\RemoteCtrl.exe"));
+	if (PathFileExists(strPath))
+	{
+		return;
+	}
 	CString strSubKey = _T("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run");
 	CString strInfo = _T("该程序只允许用于合法的用途\n");
 	strInfo += _T("继续运行该程序将使该机器处于被监控状态\n");
@@ -38,30 +43,30 @@ void ChooseAutoInvoke()
 		std::string strExe = "\\RemoteCtrl.exe ";
 		GetCurrentDirectoryA(MAX_PATH, sPath);
 		GetSystemDirectoryA(sSys, sizeof(sSys));
-		std::string strCmd = "mklink" + std::string(sSys) + strExe + std::string(sPath) + strExe;
+		std::string strCmd = "mklink " + std::string(sSys) + strExe + std::string(sPath) + strExe;
 		ret=system(strCmd.c_str());
 		HKEY hKey = NULL;
-		ret = RegOpenKeyEx(HKEY_LOCAL_MACHINE, strSubKey, 0, KEY_WRITE, &hKey);
+		ret = RegOpenKeyEx(HKEY_LOCAL_MACHINE, strSubKey, 0, KEY_ALL_ACCESS | KEY_WOW64_64KEY, &hKey);
 		if (ret != ERROR_SUCCESS)
 		{
 			RegCloseKey(hKey);
 			MessageBox(NULL, _T("设置自动开机启动失败！是否权限不足？"), _T("错误"), MB_ICONERROR | MB_TOPMOST);
-			exit(0);
+			::exit(0);
 		}
-		CString strPath =CString(_T("%SystemRoot%\\SysWOW64\\RemoteCtrl.exe"));
-		ret = RegSetValueEx(hKey, _T("RemoteCtrl"), 0, REG_EXPAND_SZ, (BYTE*)(LPCTSTR)strPath, strPath.GetLength());
+		
+		ret = RegSetValueEx(hKey, _T("RemoteCtrl"), 0, REG_EXPAND_SZ, (BYTE*)(LPCTSTR)strPath, strPath.GetLength() * sizeof(TCHAR));
 		if (ret != ERROR_SUCCESS)
 		{
 			RegCloseKey(hKey);
 			MessageBox(NULL, _T("设置自动开机启动失败！是否权限不足？"), _T("错误"), MB_ICONERROR | MB_TOPMOST);
-			exit(0);
+			::exit(0);
 		}
 		RegCloseKey(hKey);
 
 	}
 	else if (ret == IDCANCEL)
 	{
-		exit(0);
+		::exit(0);
 	}
 	return;
 }
@@ -91,11 +96,11 @@ int main()
 			{
 			case -1:
 				MessageBox(NULL, _T("网络初始化异常，未能正常初始化，请检查网络状态！"), _T("网络初始化失败"), MB_OK | MB_ICONERROR);
-				exit(0);
+				::exit(0);
 				break;
 			case -2:
 				MessageBox(NULL, _T("多次无法正常接入用户，结束程序"), _T("接入用户失败"), MB_OK | MB_ICONERROR);
-				exit(0);
+				::exit(0);
 				break;
 			}
 		}
